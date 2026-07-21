ee38cb8b0761812ab3c24f81f2f69d7ce73db534
Arya Mahendra Karade <158163047+AryaKarade@users.noreply.github.com>
2026-07-21 17:30:47 +0530

    Create darkmode.js

---
+++ js/darkmode.js
@@ -0,0 +1,104 @@
+/* =====================================================
+   DARK MODE FUNCTIONALITY
+   Arya Mahendra Karade Portfolio
+===================================================== */
+
+
+
+// Select Theme Button
+
+const themeBtn = document.getElementById("theme-toggle");
+
+
+
+
+// Load saved theme when website opens
+
+const savedTheme = localStorage.getItem("theme");
+
+
+
+if(savedTheme === "dark"){
+
+
+    document.body.classList.add("dark-mode");
+
+
+    if(themeBtn){
+
+        themeBtn.innerHTML =
+        '<i class="fa-solid fa-sun"></i>';
+
+    }
+
+
+}
+
+
+
+
+
+
+// Toggle Dark Mode
+
+
+if(themeBtn){
+
+
+
+    themeBtn.addEventListener("click",()=>{
+
+
+
+        document.body.classList.toggle("dark-mode");
+
+
+
+        const isDark =
+        document.body.classList.contains(
+            "dark-mode"
+        );
+
+
+
+        if(isDark){
+
+
+            localStorage.setItem(
+                "theme",
+                "dark"
+            );
+
+
+
+            themeBtn.innerHTML =
+            '<i class="fa-solid fa-sun"></i>';
+
+
+
+        }
+
+        else{
+
+
+            localStorage.setItem(
+                "theme",
+                "light"
+            );
+
+
+
+            themeBtn.innerHTML =
+            '<i class="fa-solid fa-moon"></i>';
+
+
+
+        }
+
+
+
+    });
+
+
+
+}
